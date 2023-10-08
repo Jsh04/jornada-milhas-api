4b248ca6b3974e44bef26063741b438ebaf26bac
Jose Silvio <josesilvio.bs@gmail.com>
2023-10-08 14:41:25 -0300

    Pequenas correções

--- API-Infraestrutura/Configuracao/ConfiguracaoElastic.cs
+++ API-Infraestrutura/Configuracao/ConfiguracaoElastic.cs
@@ -9,8 +9,8 @@ public static class ConfiguracaoElastic
     public static ElasticsearchClient CreateElasticCLient()
     {
         var settings = new ElasticsearchClientSettings(new Uri("https://localhost:9200"))
-            .CertificateFingerprint("63b51a934114b5b0dfc6846391d7305a7a659658b29c364c58875dd273ab3d25")
-            .Authentication(new BasicAuthentication("elastic", "pI0fNIHio3Y3Rahbrvos"));
+            .CertificateFingerprint("")
+            .Authentication(new BasicAuthentication("elastic", ""));
 
         var client = new ElasticsearchClient(settings);
 
