1fef54cfcb4e7755dc3119551a3925bd75c0c809
José Silvio <josesilvio.bs@gmail.com>
2024-02-15 22:08:13 -0300

    Revert "Revert "Começando a implementação do teste de interface de cadastro de destino""
    This reverts commit 126c989e7dc391f7e1fc85df2d6fa2956fc9ae20.

--- JornadaMilhasTest/InterfacesTest/PageObjects/RegisterDestinyPO.cs
+++ JornadaMilhasTest/InterfacesTest/PageObjects/RegisterDestinyPO.cs
@@ -10,7 +10,12 @@ namespace JornadaMilhasTest.InterfacesTest.PageObjects;
 public class RegisterDestinyPO
 {
     private IWebDriver driver;
-    private By 
+    private By InputFiles;
+    private By NameDestiny;
+    private By SubtitleDestiny;
+    private By PriceDestiny;
+    private By DescriptionPortuguese;
+    private By DestinationEnglish;
 
 
     public RegisterDestinyPO(IWebDriver driver)
