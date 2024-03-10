53a3d5a5f5876f7c3609672c316fde9ac54fcfb7
José Silvio <josesilvio.bs@gmail.com>
2024-03-10 13:01:15 -0300

    Correções e implementação de Midleware para exceções

--- JornadaMilhas.Application/Messagings/Senders/SendEmailMessage.cs
+++ JornadaMilhas.Application/Messagings/Senders/SendEmailMessage.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
-using JornadaMilhas.Core.DTO.Usuario;
+using System.Threading.Channels;
 using Microsoft.Extensions.Configuration;
 using RabbitMQ.Client;
 
@@ -13,7 +10,6 @@ namespace JornadaMilhas.Application.Messagings.Senders;
 public class SendEmailMessage
 {
     private readonly IConfiguration _configuration;
-    private readonly IModel _channel;
     private readonly IConnection _connection;
 
     public SendEmailMessage(IConfiguration configuration)
@@ -22,28 +18,28 @@ public class SendEmailMessage
          _connection = new ConnectionFactory
              { HostName = _configuration["RabbitMq:Host"], Port = int.Parse(_configuration["RabbitMq:Port"]) }.CreateConnection();
 
-         _channel = _connection.CreateModel();
-
-         ConfigurationOfQueueEmail();
-
-
     }
 
     public void SendConfirmMmail(string email)
     {
-        string message = JsonSerializer.Serialize(email);
+        using var channel = _connection.CreateModel();
+        ConfigurationOfQueueEmail(channel);
+
+        var message = JsonSerializer.Serialize(email);
         var body = Encoding.UTF8.GetBytes(message);
 
-        _channel.BasicPublish(exchange: "dataUser", routingKey: "email", basicProperties: null, body: body);
+        channel.BasicPublish(exchange: "dataUser", routingKey: "email", basicProperties: null, body: body);
+
     }
 
-    private void ConfigurationOfQueueEmail()
+    private void ConfigurationOfQueueEmail(IModel channel)
     {
-        _channel.ExchangeDeclare(exchange: "dataUser", type: ExchangeType.Direct);
-        _channel.QueueDeclare(queue: "email", durable: false, exclusive: false, autoDelete: false, arguments: null);
-        _channel.QueueBind(queue: "email", exchange: "dataUser", routingKey:"email");
+        channel.ExchangeDeclare(exchange: "dataUser", type: ExchangeType.Direct);
 
-        
+        channel.QueueDeclare(queue: "email", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        channel.QueueBind(queue: "email", exchange: "dataUser", routingKey:"email");
+
     }
 
 }
