64ae810e3c5b6137e877f0ae8a746773b2763346
José Silvio <josesilvio.bs@gmail.com>
2024-04-22 23:02:16 -0300

    Começando a implementação das querys

---
+++ JornadaMilhas.Application/Querys/DestinysQuerys/DestinyGetAll/GetAllDestinysQuery.cs
@@ -0,0 +1,12 @@
+﻿using JornadaMilhas.Core.Entities.Destinys;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Application.Querys.DestinysQuerys.DestinyGetAll
+{
+    public record GetAllDestinysQuery : IRequest<List<Destiny>>{}
+}
---
+++ JornadaMilhas.Application/Querys/DestinysQuerys/DestinyGetAll/GetAllDestinysQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using JornadaMilhas.Core.Entities.Destinys;
+using JornadaMilhas.Infrastruture.Persistence.UOW;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Application.Querys.DestinysQuerys.DestinyGetAll;
+
+public class GetAllDestinysQueryHandler : IRequestHandler<GetAllDestinysQuery, List<Destiny>> {
+
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetAllDestinysQueryHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public Task<List<Destiny>> Handle(GetAllDestinysQuery request, CancellationToken cancellationToken)
+    {
+        _unitOfWork.DestinoRepository.GetAllAsync(1, 10, cancellationToken);
+        throw new NotImplementedException();
+    }
+}
