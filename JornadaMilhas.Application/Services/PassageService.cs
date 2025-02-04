2b55f0ca8b3c50504974943beeebeb23ebc9e778
jsh04 <josesilvio.bs@gmail.com>
2025-02-03 21:32:38 -0300

    Add passage service file

---
+++ JornadaMilhas.Application/Services/PassageService.cs
@@ -0,0 +1,23 @@
+﻿using JornadaMilhas.Application.Commands.PassagesCommands.InputModels;
+using JornadaMilhas.Application.Commands.PassagesCommands.PaidPassage;
+using JornadaMilhas.Application.Interfaces.Services;
+using JornadaMilhas.Common.Results;
+using MediatR;
+
+namespace JornadaMilhas.Application.Services;
+
+public class PassageService : IPassageService
+{
+    private readonly ISender _mediator;
+
+    public PassageService(ISender mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    public Task<Result> PayPassagesAsync(long customerId, List<PaidPassageInputModel> passageInputModels)
+    {
+        var payPassagesCommands = new PaidPassageCommand(customerId, passageInputModels);
+        return _mediator.Send(payPassagesCommands);
+    }
+}
